b03052e59322248571a43258fc2de259ad74b869
sahandMac <sahandmac@sahandMacs-MacBook-Pro.local>
2024-03-10 22:35:08 +0330

    Modify http client

--- lib/data/common/http_client.dart
+++ lib/data/common/http_client.dart
@@ -1,3 +1,4 @@
 import 'package:dio/dio.dart';
 
 final httpClient = Dio(BaseOptions(baseUrl: 'https://api.digikala.com/v1/'));
+final httpClientSecond = Dio(BaseOptions(baseUrl: 'http://expertdevelopers.ir/api/v1'));
